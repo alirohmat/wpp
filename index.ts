39887b2f6101da50209dfa1aae3558b4187fd200
alirohmat <71525350+alirohmat@users.noreply.github.com>
2024-11-17 23:09:03 +0700

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { create, Whatsapp } from '@wppconnect-team/wppconnect';
-
-create({
-  phoneNumber: '6287834100533',
-  catchLinkCode: (str) => console.log('Code: ' + str),
-})
-  .then((client) => start(client))
-  .catch((error) => console.log(error));
