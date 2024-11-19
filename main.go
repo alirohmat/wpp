9861f0bc04c3fddfeed290128715e532264298fb
alirohmat <71525350+alirohmat@users.noreply.github.com>
2024-11-20 04:04:50 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,70 +1,73 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "github.com/tulir/whatsmeow"
-    "github.com/tulir/whatsmeow/events"
-    "github.com/tulir/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
+func eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		fmt.Println("Received a message!", v.Message.GetConversation())
+	}
+}
+
 func main() {
-    // Inisialisasi penyimpanan device
-    deviceStore, err := sqlstore.New("sqlite3", "file:example.db?_foreign_keys=on", nil)
-    if err!= nil {
-        log.Fatal("Gagal inisialisasi penyimpanan device:", err)
-    }
+	// |------------------------------------------------------------------------------------------------------|
+	// | NOTE: You must also import the appropriate DB connector, e.g. github.com/mattn/go-sqlite3 for SQLite |
+	// |------------------------------------------------------------------------------------------------------|
 
-    // Buat klien WhatsApp baru
-    client := whatsmeow.NewClient(deviceStore, nil)
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client.AddEventHandler(eventHandler)
 
-    // Pastikan klien terhubung sebelum melakukan pairing
-    if err := client.Connect(); err!= nil {
-        log.Fatal("Gagal menghubungkan klien:", err)
-    }
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				// Render the QR code here
+				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
+				fmt.Println("QR code:", evt.Code)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    // Tunggu hingga koneksi fully established (menggunakan whatsmeow/events)
-    eventoChan := make(chan whatsmeow.Event)
-    eventHandlerID := client.AddEventHandler(func(event whatsmeow.Event) {
-        eventoChan <- event
-    })
-    go func() {
-        for {
-            select {
-            case event := <-eventoChan:
-                if _, ok := event.(*events.QRCodeEvent); ok {
-                    // Koneksi sekarang sudah fully established
-                    close(eventoChan)
-                    return
-                }
-            }
-        }
-    }()
-    // Tunggu hingga event QRCodeEvent diterima
-    <-eventoChan
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
 
-    // Contoh penggunaan PairPhone()
-    phone := "6287834100533" // Nomor telepon yang ingin dipasangkan
-    showPushNotification := true
-    clientType := whatsmeow.PairClientOtherWebClient
-    clientDisplayName := "Browser (Windows)" // Format wajib: Browser (OS)
-    code, err := client.PairPhone(phone, showPushNotification, clientType, clientDisplayName)
-
-    if err!= nil {
-        switch {
-        case errors.Is(err, &whatsmeow.PairDatabaseError{}):
-            log.Println("Kesalahan Basis Data Pairing:", err)
-        case errors.Is(err, &whatsmeow.PairProtoError{}):
-            log.Println("Kesalahan Protokol Pairing:", err)
-        default:
-            log.Println("Kesalahan tidak diketahui pada PairPhone():", err)
-        }
-    } else {
-        fmt.Printf("Kode Pairing untuk %s: %s\n", phone, code)
-    }
-
-    // Jangan lupa untuk menghapus event handler jika tidak dibutuhkan lagi
-    client.RemoveEventHandler(eventHandlerID)
+	client.Disconnect()
 }
